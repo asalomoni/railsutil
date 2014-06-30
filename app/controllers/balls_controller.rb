1e80c1e860c7c73a78849191b690e73613d86ed6
Andrea Salomoni <andrea.salomoni@intersail.it>
2014-06-30 13:00:37 +0200

    Aggiunto menu secondario

--- app/controllers/balls_controller.rb
+++ app/controllers/balls_controller.rb
@@ -4,6 +4,7 @@ class BallsController < ApplicationController
   # GET /balls
   # GET /balls.json
   def index
+    sleep(0.2)
     @balls = Ball.all
     @balls = @balls.paginate(:page => params[:page], :per_page => 5)
   end
